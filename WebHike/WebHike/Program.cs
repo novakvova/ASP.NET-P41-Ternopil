86f4748bfb9db339251753cb1951c3cb541bb792
novakvova <novakvova@gmail.com>
2026-07-14 20:43:02 +0300

    Add Seed Role and User

--- WebHike/WebHike/Program.cs
+++ WebHike/WebHike/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using WebHike.Constants;
 using WebHike.Data;
+using WebHike.Data.Entities.Identity;
 using WebHike.Interfaces;
 using WebHike.Services;
 
@@ -12,6 +15,18 @@ string strConn = builder.Configuration
 
 builder.Services.AddDbContext<HikeDbContext>(opt =>
     opt.UseNpgsql(strConn));
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+    })
+    .AddEntityFrameworkStores<HikeDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IImageService, ImageOptimizationService>();
 
@@ -40,5 +55,35 @@ app.MapControllerRoute(
     pattern: "{controller=Main}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+
+//Перед запуском додам в БД ролі користувачів, якщо їх там немає
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var roleManager = services.GetRequiredService<RoleManager<RoleEntity>>();
+    var userManager = services.GetRequiredService<UserManager<UserEntity>>();
+
+    if (!roleManager.Roles.Any())
+    {
+        foreach (var roleName in Roles.ListRoles())
+        {
+            await roleManager.CreateAsync(new RoleEntity { Name = roleName });
+        }
+    }
+
+    if (!userManager.Users.Any())
+    {
+        var user = new UserEntity
+        {
+            Email="admin@gmail.com",
+            UserName = "admin@gmail.com",
+            FirstName = "Павло",
+            LastName = "Зібров",
+            Image = "default.jpg"
+        };
+        await userManager.CreateAsync(user, "Qwerty1-");
+        await userManager.AddToRoleAsync(user, Roles.Admin);
+    }
+}
 
 app.Run();
