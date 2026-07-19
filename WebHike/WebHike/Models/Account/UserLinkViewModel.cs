d519b6062485619ba738b861fe3182791b67b52b
novakvova <novakvova@gmail.com>
2026-07-19 18:50:41 +0300

    Add View Compoent user info

---
+++ WebHike/WebHike/Models/Account/UserLinkViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebHike.Models.Account;
+
+public class UserLinkViewModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+}
---
+++ WebHike/WebHike/ViewComponents/UserLinkViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebHike.Data.Entities.Identity;
+using WebHike.Models.Account;
+
+namespace WebHike.ViewComponents;
+
+public class UserLinkViewComponent(UserManager<UserEntity> userManager) 
+    : ViewComponent
+{
+    //Він не може бути асихроний
+    public IViewComponentResult Invoke()
+    {
+        var userName = User.Identity?.Name;
+        var model = new UserLinkViewModel();
+        if(userName != null)
+        {
+            var user = userManager.FindByNameAsync(userName).Result;
+            model.Name = $"{user.LastName} {user.FirstName}";
+            model.Image = $"/images/{user.Image}_64.webp";
+        }
+
+        return View(model);
+    }
+}
