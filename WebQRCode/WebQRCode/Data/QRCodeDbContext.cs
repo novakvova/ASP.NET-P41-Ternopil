6195e84529298ed6af643993e160567d8c945812
novakvova <novakvova@gmail.com>
2026-08-20 18:33:15 +0300

    Add Migration is good

---
+++ WebQRCode/WebQRCode/Data/QRCodeDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebQRCode.Data.Entities.Identity;
+
+namespace WebQRCode.Data;
+
+public class QRCodeDbContext : IdentityDbContext<UserEntity, RoleEntity, int>
+{
+    public QRCodeDbContext(DbContextOptions<QRCodeDbContext> options)
+    : base(options)
+    { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //identity
+        modelBuilder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        modelBuilder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.Role)
+            .WithMany(r => r.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+    }
+}
--- WebQRCode/WebQRCode/Program.cs
+++ WebQRCode/WebQRCode/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebQRCode.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<QRCodeDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("MyQRCodeConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
