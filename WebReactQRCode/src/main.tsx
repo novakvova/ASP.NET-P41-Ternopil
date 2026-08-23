235b5af9d1db06f4d3ad08331cfaec8dd553616c
novakvova <novakvova@gmail.com>
2026-08-23 21:04:20 +0300

    Working is good Login begin

--- WebReactQRCode/src/main.tsx
+++ WebReactQRCode/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ReactQueryProvider from './providers/ReactQueryProvider.tsx'
+import {BrowserRouter} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ReactQueryProvider>
-            <App />
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
         </ReactQueryProvider>
     </StrictMode>,
 )
